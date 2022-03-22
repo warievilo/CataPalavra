b9cc973a51ad4c7f422498d12576ba032d71d59e
Willians Oliveira <williansarievilo@gmail.com>
2022-03-22 13:08:26 -0300

    Implementado o filtro de palavras

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace Web.Controllers;
 
@@ -12,8 +13,82 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string mascara, string letrasIgnoradas, string letrasObrigatorias)
     {
-        return View();
+        ViewData["mascara"] = mascara;
+        ViewData["letrasIgnoradas"] = letrasIgnoradas;
+        ViewData["letrasObrigatorias"] = letrasObrigatorias;
+
+        List<string> words = Search(mascara, letrasIgnoradas, letrasObrigatorias);
+
+        int nSize = 6;
+        var list = new List<List<string>>(); 
+
+        for (int i = 0; i < words.Count; i += nSize) 
+            list.Add(words.GetRange(i, Math.Min(nSize, words.Count - i))); 
+
+        return View(list);
+    }
+
+    private List<string> Search(string? inputedWord, string? ignoredLetters, string? requiredLetters)
+    {
+        var words = new List<string>();
+
+        if (string.IsNullOrEmpty(inputedWord))
+            return words;
+
+        var ra = GetRegexPrimaryFilter(inputedWord, ignoredLetters);
+
+        var rb = GetRegexSecondaryFilter(requiredLetters);
+        
+        //string[] dicio = System.IO.File.ReadAllLines(@".\dicio\pt-br-sem-acentos\br-sem-acentos.txt");
+        string[] dicio = System.IO.File.ReadAllLines(@".\dicio\pt-br-master\dicio");
+        
+        foreach (string word in dicio)
+        {
+            if (ra.IsMatch(word) && rb.IsMatch(word))
+                words.Add(word);
+        }
+
+        return words;
+    }
+
+    private Regex GetRegexPrimaryFilter(string inputedWord, string? ignoredLetters) 
+    {
+        string ignoredLettersFilter = string.Empty;
+        
+        if (!string.IsNullOrEmpty(ignoredLetters) && ignoredLetters.Length > 0)
+            ignoredLettersFilter = $"(?![{ ignoredLetters.ToLower() }])";
+
+        string lettersfilter = string.Empty;
+
+        foreach (char c in inputedWord.ToLower().ToCharArray())
+        {
+            var wordFilter = ".";
+
+            if (c != '*')
+                wordFilter = $"[{ c }]";
+
+            if (string.IsNullOrEmpty(ignoredLettersFilter))
+                lettersfilter += wordFilter;
+            else
+                lettersfilter += $"{ignoredLettersFilter}{wordFilter}";        
+        }
+
+        lettersfilter = @$"\b{ lettersfilter }\b";
+
+        return new Regex(lettersfilter, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    }
+
+    private Regex GetRegexSecondaryFilter(string? requiredLetters) 
+    {
+        //(?=\w*c)(?=\w*y)\w+
+        var filter = @"\w+";
+
+        if (!string.IsNullOrEmpty(requiredLetters))
+            foreach (var c in requiredLetters.ToLower().ToCharArray())
+                filter = $@"(?=\w*{ c }){ filter }";
+
+        return new Regex(filter, RegexOptions.Compiled | RegexOptions.IgnoreCase);
     }
 }
