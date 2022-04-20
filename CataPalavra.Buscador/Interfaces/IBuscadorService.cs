c118cdac4646e3489fc5373021217358d7a19e2b
Willians Vieira Oliveira <williansarievilo@gmail.com>
2022-04-20 12:50:49 -0300

    Refactor + Correção de bugs.
    Refactor + Correção de bugs.

--- CataPalavra.Buscador/Interfaces/IBuscadorService.cs
+++ CataPalavra.Buscador/Interfaces/IBuscadorService.cs
@@ -2,5 +2,5 @@ namespace CataPalavra.Buscador.Interfaces;
 
 public interface IBuscadorService
 {
-    IEnumerable<string> Buscar(string? mascara, string? letrasIgnoradas, string? letrasObrigatorias);
+    IEnumerable<string> Search(string? mascara, string? letrasIgnoradas, string? letrasObrigatorias);
 }
